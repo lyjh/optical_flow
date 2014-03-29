05db190f94e7cdf5613f6398db853039853343b2
tsechiw <tsechiw@deeplearn4.eecs.umich.edu>
2014-03-29 16:30:42 -0400

    first commit

---
+++ calculateFlow.m
@@ -0,0 +1,20 @@
+d = dir('../Dataset/woman/*.jpg');
+N = size(d,1);
+im = imread(['../Dataset/woman/' d(1).name]);
+sz = size(im);
+imgs = zeros([sz, N]);
+uv = zeros([sz(1:2), 2, N]);
+disp('Loading data...');
+for i=1:N
+    imgs(:,:,:,i) = imread(['../Dataset/woman/' d(i).name]);
+end
+
+disp('Start calculating flow...');
+for i=2:N
+    tic;
+    uv(:,:,:,i) = estimate_flow_interface(imgs(:,:,:,i-1), imgs(:,:,:,i));
+    toc;
+   fprintf('Process %d/%d frames', i, N-1);
+end
+
+save('woman_flow', 'uv');
