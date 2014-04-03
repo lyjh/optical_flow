b358d7371cc10a6f26cb7893c7fd17dc1fdf9dcd
Tse-Chi Wang <tsechiw@caen-vnc10.engin.umich.edu>
2014-04-03 19:51:46 -0400

    push

--- calculateFlow2.m
+++ calculateFlow2.m
@@ -30,54 +30,58 @@ function [] = calculateFlow2(title)
 	savedRes = [];
     hold on
 	disp('Start calculating flow...');
+	pause;
 	for i=2:N
         region = round([bbox(1)-0.25*bbox(3), bbox(2)-0.25*bbox(4), 1.5*bbox(3), 1.5*bbox(4)]);
         region = sanityCheck(region, w, h);
         im1 = imgs(:,:,:,i-1);
         im2 = imgs(:,:,:,i);
-        %im1_crop = im1(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1,:);
-        %im2_crop = im2(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1,:);
+        im1_crop = im1(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1,:);
+        im2_crop = im2(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1,:);
 		tic;
-		[flowx, flowy] = Coarse2FineTwoFrames(im1, im2, para);
+		[flowx, flowy] = Coarse2FineTwoFrames(im1_crop, im2_crop, para);
 		toc;
-	    flowx_mean = mean(mean(flowx));
-        flowy_mean = mean(mean(flowy));
-        net_flow_mag = sqrt((flowx-flowx_mean).^2+(flowy-flowy_mean).^2);
+	    %flowx_mean = mean(mean(flowx));
+        %flowy_mean = mean(mean(flowy));
+        %net_flow_mag = sqrt((flowx-flowx_mean).^2+(flowy-flowy_mean).^2);
         
         fprintf('Process %d/%d frames\n', i-1, N-1);
         
         
         clf
-        %diff = bbox(1:2)-region(1:2)+1;
-        %flowx_bg = (sum(sum(flowx))-sum(sum(flowx(diff(2):diff(2)+bbox(4)-1,diff(1):diff(1)+bbox(3)-1))))/(region(3)*region(4)-bbox(3)*bbox(4));
-        %flowy_bg = (sum(sum(flowy))-sum(sum(flowy(diff(2):diff(2)+bbox(4)-1,diff(1):diff(1)+bbox(3)-1))))/(region(3)*region(4)-bbox(3)*bbox(4));
+        diff = bbox(1:2)-region(1:2)+1;
+        flowx_bg = (sum(sum(flowx))-sum(sum(flowx(diff(2):diff(2)+bbox(4)-1,diff(1):diff(1)+bbox(3)-1))))/(region(3)*region(4)-bbox(3)*bbox(4));
+        flowy_bg = (sum(sum(flowy))-sum(sum(flowy(diff(2):diff(2)+bbox(4)-1,diff(1):diff(1)+bbox(3)-1))))/(region(3)*region(4)-bbox(3)*bbox(4));
         
-        %net_flow_mag = zeros(h, w);
-        %net_flow_mag(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1) = ...
-        %    sqrt((flowx-repmat(flowx_bg,[region(4) region(3)])).^2+(flowy-repmat(flowy_bg,[region(4) region(3)])).^2);
+        net_flow_mag = zeros(h, w);
+        net_flow_mag(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1) = ...
+            sqrt((flowx-repmat(flowx_bg,[region(4) region(3)])).^2+(flowy-repmat(flowy_bg,[region(4) region(3)])).^2);
         
+        subplot(2,1,1);
         imagesc(net_flow_mag);
         %bboxS = reshape(net_flow_mag(bbox(2):bbox(2)+bbox(4)-1,bbox(1):bbox(1)+bbox(3)-1), bbox(3)*bbox(4),1);
         %bboxL = reshape(net_flow_mag(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1), region(3)*region(4),1);
-        [clusters, C] = kmeans(reshape(net_flow_mag, h*w, 1), 2, 'start', 'uniform');
-        %clusters = reshape(clusters, h, w);
+        [clusters, C] = kmeans(reshape(net_flow_mag(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1), region(3)*region(4), 1), 2, 'start', 'uniform');
+        clusters = reshape(clusters, region(4), region(3));
         [thres, c] = max(C);
         %sort_flow_mag = sort(bboxL,'descend');
         %cumSum = cumsum(sort_flow_mag)./sum(bboxL);
         %thres = sort_flow_mag(find(cumSum > .25, 1, 'first' ));
-        %[r, c] = find(clusters == c);
-        %x0 = min(c);
-        %x1 = max(c);
-        %y0 = min(r);
-        %y1 = max(r);
-        x0 = find(max(net_flow_mag)>thres, 1 );
-        x1 = find(max(net_flow_mag)>thres, 1, 'last' );
-        y0 = find(max(net_flow_mag,[],2)>thres, 1 );
-        y1 = find(max(net_flow_mag,[],2)>thres, 1, 'last' );
+        [r, c] = find(clusters == c);
+        x0 = min(c);
+        x1 = max(c);
+        y0 = min(r);
+        y1 = max(r);
+        %x0 = find(max(net_flow_mag(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1))>thres, 1 );
+        %x1 = find(max(net_flow_mag(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1))>thres, 1, 'last' );
+        %y0 = find(max(net_flow_mag(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1),[],2)>thres, 1 );
+        %y1 = find(max(net_flow_mag(region(2):region(2)+region(4)-1, region(1):region(1)+region(3)-1),[],2)>thres, 1, 'last' );
         
+        subplot(2,1,2);
+        imshow(im1);
         savedRes = [savedRes; bbox];
-        bbox = [x0, y0, x1-x0, y1-y0];
-        rectangle('Position', bbox, 'EdgeColor', 'g','LineWidth', 2.5)
+        bbox = [region(1)+x0, region(2)+y0, x1-x0, y1-y0];
+        rectangle('Position', bbox, 'EdgeColor', 'g','LineWidth', 2.5);
         drawnow;
 	end
 
